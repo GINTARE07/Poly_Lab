5522daaf8102246c5071993622bed0b49411a45c
james yates <yates_7@hotmail.com>
2023-01-23 15:29:17 +0000

    Lab completed

---
+++ polymorphism_lab_start/src/main/java/Wallet.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+
+public class Wallet {
+    private String name;
+    private ArrayList<IScan> itemsInWallet;
+
+    public Wallet(String name) {
+        this.name = name;
+        this.itemsInWallet = new ArrayList<IScan>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getNumberOfItems() {
+        return this.itemsInWallet.size();
+    }
+
+    public void addItem(IScan item) {
+        this.itemsInWallet.add(item);
+    }
+
+}
